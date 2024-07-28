857947e4ee22cafd32da438821af01e3af0c435d
unknown <muzangwance@gmail.com>
2024-07-28 13:55:49 +0200

    first commit

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+  
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>E COMMERCE</title>
+  </head>
+  <body>
+    <div class="container-scroller">
+     >
+        <div class="col-md-12 p-0 m-0">
+          <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
+            <div class="ps-lg-1">
+              <div class="d-flex align-items-center justify-content-between">
+                
+                <a href="https://www.bootstrapdash.com/product/purple-bootstrap-admin-template/?utm_source=organic&utm_medium=banner&utm_campaign=buynow_demo" target="_blank" class="btn me-2 buy-now-btn border-0"></a>
+              </div>
+            </div>
+            <div class="d-flex align-items-center justify-content-between">
+              <a href="https://www.bootstrapdash.com/product/purple-bootstrap-admin-template/"><i class="mdi mdi-home me-3 text-white"></i></a>
+              <button id="bannerClose" class="btn border-0 p-0">
+                <i class="mdi mdi-close text-dark me-0"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      @include('admin.navbar')
+
+      @include('admin.sidebar')
+
+     
+      @include('admin.body')
+
+    
+
+
+
+       
+
+        </div>
+        <!-- main-panel ends -->
+      </div>
+      <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <!-- End custom js for this page -->
+
+      @include('admin.scripts')
+  </body>
+</html>
---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,6 @@
+
+  <div class="cpy_">
+     <p class="mx-auto">© <?php echo date("Y"); ?> Copyright.<a href="https://html.design/"> E commerce</a><br>
+           
+   </p>
+</div>
